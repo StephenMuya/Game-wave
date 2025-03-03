9422790d74ab8150ed38cee99391b1029399516d
StephenMuya <stephen2020mutheu@gmail.com>
2025-03-03 18:45:16 +0300

    updated

--- src/Landingpage.jsx
+++ src/Landingpage.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
-
+import React from "react";
+import styles from './styles.module.css'
 
 function Landing() {
+    return(
+    <nav className={styles.nav}>
+      <header>Game Wave</header>
   
-}
+      <ul>
+          <li>Login</li>
+          <li>Cart</li>
+          <li>Contacts</li>
+      </ul>
+    </nav>
+    );
+  }
 
-export default Landing
+export default Landing;
