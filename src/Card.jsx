cc4aace87a548aaf0a60383dd261a1f402470927
StephenMuya <stephen2020mutheu@gmail.com>
2025-03-06 16:19:32 +0300

    Boostrap card included

---
+++ src/Card.jsx
@@ -0,0 +1,65 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import styles from './card.module.css'
+
+function Card() {
+    return(
+    <section className={styles.container}>
+<div className="card" style={{width: '18rem'}}>
+  <img src="https://via.placeholder.com/150" className="card-img-top" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div className="card" style={{width: '18rem'}}>
+  <img src="https://via.placeholder.com/150" className="card-img-top" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div className="card" style={{width: '18rem'}}>
+  <img src="https://via.placeholder.com/150" className="card-img-top" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div className="card" style={{width: '18rem'}}>
+  <img src="https://via.placeholder.com/150" className="card-img-top" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div className="card" style={{width: '18rem'}}>
+  <img src="https://via.placeholder.com/150" className="card-img-top" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div className="card" style={{width: '18rem'}}>
+  <img src="https://via.placeholder.com/150" className="card-img-top" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</section>
+)
+}
+
+export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Landing from "./Landingpage"; 
-
+import Landing from "./Landing"
+import Card from "./Card"
 
 const root = createRoot(document.getElementById("root"));
-root.render(<Landing />);
+root.render(
+    <>
+<Landing />
+<Card />
+</>
+);
