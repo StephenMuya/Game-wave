9db9492f0f21362e9205190361b60e19f49716cf
StephenMuya <stephen2020mutheu@gmail.com>
2025-04-02 19:08:21 +0300

    picks cards

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,10 +4,77 @@ import style from './card.module.css'
 function Card() {
     return(
     <>
-      <h1>This should be a card</h1>
+      <section className={style.container}>
+        <article className={style.pick_1}>
+          <figure>
+            <img src="" alt="" />
+          </figure>
+
+          <aside>
+            <header>Game Name</header>
+            <p>Game description</p>
+          </aside>
+        </article>
+
+        <article className={style.pick_2}>
+          <figure>
+            <img src="" alt="" />
+          </figure>
+
+          <aside>
+            <header>Game Name</header>
+            <p>Game description</p>
+          </aside>
+        </article>
+
+        <article className={style.pick_3}>
+          <figure>
+            <img src="" alt="" />
+          </figure>
+
+          <aside>
+            <header>Game Name</header>
+            <p>Game description</p>
+          </aside>
+        </article>
+
+        <article className={style.pick_4}>
+          <figure>
+            <img src="" alt="" />
+          </figure>
+
+          <aside>
+            <header>Game Name</header>
+            <p>Game description</p>
+          </aside>
+        </article>
+
+        <article className={style.pick_5}>
+          <figure>
+            <img src="" alt="" />
+          </figure>
+
+          <aside>
+            <header>Game Name</header>
+            <p>Game description</p>
+          </aside>
+        </article>
+
+        <article className={style.pick_6}>
+          <figure>
+            <img src="" alt="" />
+          </figure>
+
+          <aside>
+            <header>Game Name</header>
+            <p>Game description</p>
+          </aside>
+        </article>
+      </section>
     </>
 )
 
 }
 
 export default Card;
+
